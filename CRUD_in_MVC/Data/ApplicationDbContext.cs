6417366843e2dff67b0b5ad4e8adf1c4348ae96f
Mihir-21 <trambadiyamihir635@gmail.com>
2024-10-27 10:59:05 +0100

    Add project files.

---
+++ CRUD_in_MVC/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using CRUD_in_MVC.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_in_MVC.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
---
+++ CRUD_in_MVC/Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿namespace CRUD_in_MVC.Models
+{
+    public class Employee
+    {
+        public int  Id{ get; set; }
+
+        public string name { get; set; }
+
+        public string email { get; set; }
+
+        public string phone { get; set; }
+
+    }
+}
